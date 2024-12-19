76025b1a79d18cdc11fcc651936f9463dd3234c1
jonathan-alegre <alegrejonathan.93@gmail.com>
2024-12-18 23:53:31 -0300

    uso de ngIf

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, input, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 import { RequiredValidator } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "course-card",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",  
 })
@@ -26,5 +27,9 @@ export class CourseCardComponent {
     console.log("card component - button clicked");
 
     this.courseEmitter.emit(this.course);
+  }
+
+  isImageVisible(){
+    return this.course && this.course.iconUrl;
   }
 } 
