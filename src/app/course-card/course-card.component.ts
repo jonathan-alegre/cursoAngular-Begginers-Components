60aaa4f940d36de4dd52d88e37072ea3a75c61a1
jonathan-alegre <alegrejonathan.93@gmail.com>
2024-12-19 19:21:28 -0300

    uso de ngClass

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -2,34 +2,45 @@ import { Component, EventEmitter, input, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 import { RequiredValidator } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { ReturnStatement } from "@angular/compiler";
 
 @Component({
   selector: "course-card",
   imports: [CommonModule],
   templateUrl: "./course-card.component.html",
-  styleUrl: "./course-card.component.css",  
+  styleUrl: "./course-card.component.css",
 })
-export class CourseCardComponent {  
-  @Input(
-    {
-      required:true
-    }
-  ) 
+export class CourseCardComponent {
+  @Input({
+    required: true,
+  })
   course: Course;
 
-  @Input({required:true})
+  @Input({ required: true })
   index: Number;
 
-  @Output('courseSelected')
+  @Output("courseSelected")
   courseEmitter = new EventEmitter<Course>();
 
-  onCourseViewed(){
+  onCourseViewed() {
     console.log("card component - button clicked");
 
     this.courseEmitter.emit(this.course);
   }
 
-  isImageVisible(){
+  isImageVisible() {
     return this.course && this.course.iconUrl;
   }
-} 
+
+  cardClasses() {
+
+    return this.course.category.toLowerCase(); //string
+
+    if (this.course.category == 'BEGINNER')
+    {
+      return ['beginner']; //array
+    }
+
+    return { 'beginner': this.course.category == 'BEGINNER' }; //object configuration
+  }
+}
